8f196592422d79e1c2d8c0d9324cff0f935b7e00
VAISHNAVEP <vaishnavclt78@gmail.com>
2024-06-16 17:32:20 +0530

    Commit

--- index.js.js
+++ index.js.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const UserRoutes = require("../Routers/UserRoutes");
+const UserRoutes = require("./Routers/UserRoutes");
 require("dotenv").config();
 
 
